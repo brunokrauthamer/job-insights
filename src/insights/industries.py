36a5335d73ab33df281a88f73664189abaaa36ab
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-03-03 00:20:59 -0300

    req03

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,4 +1,26 @@
 from typing import List, Dict
+import csv
+
+def read(path: str) -> List[Dict]:
+    """Reads a file from a given path and returns its contents
+
+    Parameters
+    ----------
+    path : str
+        Full path to file
+
+    Returns
+    -------
+    list
+        List of rows as dicts
+    """
+    with open(path) as data_file:
+        data_list = csv.DictReader(data_file)
+        data = []
+        for d in data_list:
+            data.append(d)
+
+    return data
 
 
 def get_unique_industries(path: str) -> List[str]:
@@ -16,7 +38,13 @@ def get_unique_industries(path: str) -> List[str]:
     list
         List of unique industries
     """
-    raise NotImplementedError
+    data = read(path)
+    unique_industries = set()
+    for d in data:
+        if d["industry"] != '':
+            unique_industries.add(d["industry"])
+    list_unique_industries = list(unique_industries)
+    return list_unique_industries
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:

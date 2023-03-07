717a9f7431bd77116b62c3a9996c5907a05f0196
Bruno Krauthamer <brunokrauthamer@gmail.com>
2023-03-06 22:38:12 -0300

    req11

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,12 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
+from unittest.mock import patch
 
+
+def function_mock(path):
+    return [{"titulo": "Maquinista", "salario": "2000", "tipo": "trainee"}]
 
 def test_brazilian_jobs():
-    pass
+    jobs_en = [{"title": "Maquinista", "salary": "2000", "type": "trainee"}]
+    with patch("src.pre_built.brazilian_jobs.jobs.read", function_mock):
+        result = read_brazilian_file("any path")
+        assert result == jobs_en
